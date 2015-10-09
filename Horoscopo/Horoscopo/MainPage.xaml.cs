849cf4a2532b8f6e8359d6ae16dde9792aae3283
everton de freitas vasconcelos <evertonfreitas22@live.com>
2015-10-08 21:41:41 -0300

    icone adicionado

--- Horoscopo/Horoscopo/MainPage.xaml.cs
+++ Horoscopo/Horoscopo/MainPage.xaml.cs
@@ -45,8 +45,7 @@ namespace Horoscopo
             WebClient webClient = new WebClient();
             webClient.OpenReadCompleted += WebClient_OpenReadCompleted;
             webClient.OpenReadAsync(new Uri("http://mftc.cf/everton/horoscopo.xml"));
-
-
+            
         }
 
         private void WebClient_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
@@ -54,28 +53,31 @@ namespace Horoscopo
 
 
             XDocument doc = XDocument.Load(e.Result);
-
-
-            using (var db = new HoroscopoContext())
+            
+           
+               using (var db = new HoroscopoContext())
             {
-                
-                foreach (var item in doc.Descendants("signo"))
+               
+
+ 
+
+            foreach (var item in doc.Descendants("signo"))
                 {
-                    
                     Horoscopo horoscopo = new Horoscopo();
+
                     horoscopo.Nome = (string)item.Element("nome");
                     horoscopo.Data = (string)item.Element("periodo");
                     horoscopo.Mensagem = (string)item.Element("msg");
                     horoscopo.Icone = (string)item.Element("icone");
-                 
 
                     db.signos.InsertOnSubmit(horoscopo);
                     db.SubmitChanges();
+                        
 
-                }
-                
+                    }
+               
             }
-
+            
             CarregarHoroscopo();
         }
 
